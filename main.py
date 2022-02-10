e1cadfe810eae9dd69ca78719a764a53324232b1
David Oliveira <davidltoliveira@gmail.com>
2022-02-10 19:57:11 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# Download do Python em www.python.org, ao instalar marque opção Add Python 'versão' to PATH
+# Download do VSCode em code.visualstudio.com, next, next... finish
+
+print('Hello World')
